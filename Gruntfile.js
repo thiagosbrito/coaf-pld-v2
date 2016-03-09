47f506444f649c32e9095bab65bad2bae2c766c0
thiagosbrito <thiago83brito@hotmail.com>
2016-03-09 15:07:04 -0300

    Changes at Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,8 @@ module.exports = function (grunt) {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35729,
+        open: 'http://localhost:<%= connect.options.port %>'
       },
       // courtesy of Phubase Tiewthanom
       livereload: {
