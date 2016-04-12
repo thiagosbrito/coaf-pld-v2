5bd53a20cc41e1bdf141a8b6165c275e83d793fa
Cristiano Andrade <cristiano.s.andrade@gmail.com>
2016-04-12 16:28:45 -0300

    Update constants.js

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -1,10 +1,10 @@
 'use strict';
 angular.module('wbaApp')
   .constant('baseUrl',{
-    "apiUrl"          :     "http://localhost:8000/sso-wba/v1",
-    "apiEmpresas"     :     "http://192.168.99.100:8081/empresas/v1",
-    "apiComercial"    :     "http://192.168.99.100:9090/comercial/v1",
-    "apiOperacoes"    :     "http://192.168.99.100:8100/operacoes/v1"
+    "apiUrl"          :     "http://api.erp.idtrust.com.br:9000/sso-wba/v1",
+    "apiEmpresas"     :     "http://api.erp.idtrust.com.br:9000/empresas/v1",
+    "apiComercial"    :     "http://api.erp.idtrust.com.br:9000/comercial/v1",
+    "apiOperacoes"    :     "http://api.erp.idtrust.com.br:9000/operacoes/v1"
   })
   .constant('nanoScrollerDefaults', {
     nanoClass: 'scroll-pane',
